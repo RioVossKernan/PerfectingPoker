0c12f99f7931fbfbceb946e31b9fc73628a6ef40
RioVossKernan <riov-k@comcast.net>
2025-05-07 17:42:14 -0400

    feat: working camera read classifications with cropped input CNN

--- camera_read.py
+++ camera_read.py
@@ -2,53 +2,56 @@ import cv2
 import tensorflow as tf
 import numpy as np
 import nbimporter
-from dual_model import CardPredictor
+from cropped_img_model import CardModel, crop_and_warp
 
 # ----------------------------------------------------------------------
 # Load the pre-trained models
 
-suit_model = CardPredictor(4)
-suit_model.build((None, 300, 300, 3))
+suit_model = CardModel(4)
 suit_model.compile(
     optimizer='adam',
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
-suit_model.load_weights("suit_model_ckpt.keras")
+suit_model.build(input_shape=(1, 80, 60, 3))
+suit_model.load_weights("cropped_suit_model.weights.h5")
 
 
-rank_model = CardPredictor(13)
-rank_model.build((None, 300, 300, 3))
+rank_model = CardModel(13)
 rank_model.compile(
     optimizer='adam',
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
-rank_model.load_weights("rank_model_ckpt.keras")
+rank_model.build(input_shape=(1, 80, 60, 3))
+rank_model.load_weights("cropped_rank_model.weights.h5")
 
 # ----------------------------------------------------------------------
 
-def center_crop_and_resize(image, target_size=(300, 300)):
-    h, w = image.shape[:2]
-    
-    # Find the shorter dimension and crop to a centered square
-    min_dim = min(h, w)
-    start_x = (w - min_dim) // 2
-    start_y = (h - min_dim) // 2
-    cropped = image[start_y:start_y + min_dim, start_x:start_x + min_dim]
-
-    # Resize to the target size (e.g., 300x300)
-    resized = cv2.resize(cropped, target_size, interpolation=cv2.INTER_AREA)
-    return resized
 
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+
+def load_and_preprocess_frame(frame):
+    img = crop_and_warp(frame) # crop and warp
+    img = cv2.resize(img, (250, 350)) # resize to 250x350
+    
+    rank_img = img[:80,:60,:] 
+    rank_img = cv2.cvtColor(cv2.cvtColor(rank_img, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
+    rank_img = np.expand_dims(rank_img, axis=0)
+    rank_img = rank_img.astype(np.float32) / 255.0
+    
+    suit_img = img[80:160,:60,:]
+    suit_img = np.expand_dims(suit_img, axis=0)
+    suit_img = suit_img.astype(np.float32) / 255.0
+    
+    return rank_img, suit_img
 
 # ----------------------------------------------------------------------
 
 # Open the default camera (usually camera 0)
 #cam = cv2.VideoCapture(2) # This is the index of the Iphone's camera, specific for Member2's Iphone, If you are testing with different Cameras, chaneg the index
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(1)
 
 # Check if the camera opened successfully
 if not cam.isOpened():
@@ -76,14 +79,14 @@ while True:
         print("Capturing frame...")
 
         # Preprocess the frame
-        image = tf.convert_to_tensor(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        image = center_crop_and_resize(image.numpy(), target_size=(300, 300))
-        image = tf.cast(image, tf.float32) / 255.0
-        image = np.expand_dims(image, axis=0)
+        img = crop_and_warp(frame) # crop and warp
+        img = cv2.resize(img, (250, 350)) # resize to 250x350
+        
+        rank_img, suit_img = load_and_preprocess_frame(frame)
 
         # Predict
-        suit_n = tf.argmax(suit_model.predict(image), axis=1).numpy()[0]
-        rank_n = tf.argmax(rank_model.predict(image), axis=1).numpy()[0]
+        suit_n = tf.argmax(suit_model.predict(suit_img), axis=1).numpy()[0]
+        rank_n = tf.argmax(rank_model.predict(rank_img), axis=1).numpy()[0]
         
         suit_name = suits[suit_n]
         rank_name = ranks[rank_n]
